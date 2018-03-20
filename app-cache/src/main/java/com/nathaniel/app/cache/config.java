7a507ff57f03265a52fed29c0879b8fc05eeb04f
nathaniel <nathaniel.weixin@gmail.com>
2018-03-20 10:29:48 +0800

    增加重试依赖

---
+++ app-cache/src/main/java/com/nathaniel/app/cache/config.java
@@ -0,0 +1,4 @@
+package com.nathaniel.app.cache;
+
+public class config {
+}
---
+++ app-cache/src/main/java/com/nathaniel/app/cache/util/CacheImporter.java
@@ -0,0 +1,13 @@
+package com.nathaniel.app.cache.util;
+
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.context.annotation.ImportBeanDefinitionRegistrar;
+import org.springframework.core.type.AnnotationMetadata;
+
+public class CacheImporter implements ImportBeanDefinitionRegistrar {
+    @Override
+    public void registerBeanDefinitions(AnnotationMetadata annotationMetadata, BeanDefinitionRegistry beanDefinitionRegistry) {
+        BeanDefinitionBuilder builder = BeanDefinitionBuilder.genericBeanDefinition(this.getClass());
+    }
+}
